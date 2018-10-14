14c692217e265df92a43f03ded2b041ecf2971f1
visyeii <vis.yeii@gmail.com>
2018-10-14 13:45:14 +0900

    高速実行追加

--- Monte_Carlo_PI/Monte_Carlo_PI/Form1.Designer.cs
+++ Monte_Carlo_PI/Monte_Carlo_PI/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -74,6 +75,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label6);
@@ -130,11 +132,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 55);
+            this.button1.Location = new System.Drawing.Point(9, 18);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(137, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "低速実行";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -174,6 +176,16 @@
             this.label8.TabIndex = 8;
             this.label8.Text = "PI = in / (in + out) * 4";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "高速実行";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -205,6 +217,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
     }
 }
 
